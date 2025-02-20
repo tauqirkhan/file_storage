75c8973d56896fce703b72d884b5b44e5b3c1ec3
Tauqir <tauqirkhann.11@gmail.com>
2025-02-20 12:48:46 +0530

    style: add style on folder section on main
    also add new way of adding folder name

--- public/eventListeners/eventListener.js
+++ public/eventListeners/eventListener.js
@@ -2,9 +2,38 @@ const loginDialog = document.querySelector("#loginDialog");
 const signInDialog = document.querySelector("#signInDialog");
 const loginBtnNodeList = document.querySelectorAll(".loginBtn");
 const signInBtnNodeList = document.querySelectorAll(".signInBtn");
+const openFolderNameInputBtn = document.querySelector(
+  "#openFolderNameInputBtn"
+);
+const folderNameInput = document.querySelector(".hidden-input");
+const folderNameForm = document.querySelector(".folderNameForm");
 
 addEventListenerToNodeList(loginBtnNodeList, loginDialog);
 addEventListenerToNodeList(signInBtnNodeList, signInDialog);
+
+openFolderNameInputBtn.addEventListener("click", () => {
+  folderNameInput.classList.add("show");
+  folderNameInput.focus();
+});
+
+folderNameInput.addEventListener("blur", () => {
+  submitForm(folderNameInput, folderNameForm);
+});
+
+folderNameInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault(); // Prevent default form submission (to ensure blur triggers submission)
+    submitForm(folderNameInput, folderNameForm);
+  }
+});
+
+function submitForm(folderNameInput, folderNameForm) {
+  if (folderNameInput.value.trim() !== "") {
+    folderNameForm.submit();
+  } else {
+    folderNameInput.classList.remove("show");
+  }
+}
 
 function addEventListenerToNodeList(nodeList, dialog) {
   if (dialog && nodeList.length > 0) {
